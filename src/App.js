8ed2b2e6eb0698ad6b61de64be04767a55df80db
Dominic Francis <dominicf09@gmail.com>
2023-08-16 16:28:18 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
@@ -90,16 +90,21 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
-      description = "Go to move #" + move;
+    if (move === currentMove) {
+      description = "You are at move #" + move;
+      return <li key={move}>{description}</li>;
     } else {
-      description = "Go to game start";
+      if (move > 0) {
+        description = "Go to move #" + move;
+      } else {
+        description = "Go to game start";
+      }
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
     }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
   });
 
   return (
